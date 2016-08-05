2e6469c63115c63055a15fd92c887beff9055e99
OmuraHayato <sonixquria+slack@gmail.com>
2016-08-05 19:41:52 +0900

    Add files via upload

---
+++ EncDec.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class EncDec
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.InputTextBox = New System.Windows.Forms.RichTextBox()
+        Me.OutPutTextBox = New System.Windows.Forms.RichTextBox()
+        Me.Encryption_Button = New System.Windows.Forms.Button()
+        Me.Decryption_Button = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'InputTextBox
+        '
+        Me.InputTextBox.Location = New System.Drawing.Point(67, 27)
+        Me.InputTextBox.Name = "InputTextBox"
+        Me.InputTextBox.Size = New System.Drawing.Size(166, 92)
+        Me.InputTextBox.TabIndex = 1
+        Me.InputTextBox.Text = ""
+        '
+        'OutPutTextBox
+        '
+        Me.OutPutTextBox.Location = New System.Drawing.Point(68, 144)
+        Me.OutPutTextBox.Name = "OutPutTextBox"
+        Me.OutPutTextBox.Size = New System.Drawing.Size(165, 92)
+        Me.OutPutTextBox.TabIndex = 2
+        Me.OutPutTextBox.Text = ""
+        '
+        'Encryption_Button
+        '
+        Me.Encryption_Button.Location = New System.Drawing.Point(248, 27)
+        Me.Encryption_Button.Name = "Encryption_Button"
+        Me.Encryption_Button.Size = New System.Drawing.Size(64, 28)
+        Me.Encryption_Button.TabIndex = 3
+        Me.Encryption_Button.Text = "暗号化"
+        Me.Encryption_Button.UseVisualStyleBackColor = True
+        '
+        'Decryption_Button
+        '
+        Me.Decryption_Button.Location = New System.Drawing.Point(248, 74)
+        Me.Decryption_Button.Name = "Decryption_Button"
+        Me.Decryption_Button.Size = New System.Drawing.Size(64, 28)
+        Me.Decryption_Button.TabIndex = 4
+        Me.Decryption_Button.Text = "復号化"
+        Me.Decryption_Button.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(14, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 12)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "入力"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(14, 147)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 12)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "出力"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.ClientSize = New System.Drawing.Size(324, 261)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Decryption_Button)
+        Me.Controls.Add(Me.Encryption_Button)
+        Me.Controls.Add(Me.OutPutTextBox)
+        Me.Controls.Add(Me.InputTextBox)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents InputTextBox As RichTextBox
+    Friend WithEvents OutPutTextBox As RichTextBox
+    Friend WithEvents Encryption_Button As Button
+    Friend WithEvents Decryption_Button As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ EncDec.vb
@@ -0,0 +1,144 @@
+﻿Public Class EncDec
+
+    ''' <summary>復号化フラグ</summary>
+    Private isDec As Boolean = False
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+    ''' <summary>
+    ''' 暗号化、復号化を行う
+    ''' </summary>
+    Private Sub EncDec()
+
+        '16進数に変換した文字を格納
+        Dim hexStr As String = ""
+        '1文字分変換した16進数を格納
+        Dim hexStr2 As String = ""
+
+        Dim result As String = ""
+
+        Dim byteStr As Byte()
+        Dim byteStr2 As Byte()
+
+        Dim count As Integer = 0
+        Dim count2 As Integer = 0
+
+        '初期化
+        OutPutTextBox.Clear()
+
+        'UTF8のバイトデータに変換
+        byteStr = System.Text.Encoding.UTF8.GetBytes(InputTextBox.Text)
+
+        For i As Integer = 0 To byteStr.Length - 1
+            '16進数に変換
+            hexStr = Hex(byteStr(i))
+            hexStr2 &= hexStr
+            count += 1
+
+            '1文字分変換された時に16進数の数値を変更する
+            If count = 3 Then
+
+                '文字コードを変更する
+                byteStr2 = CharCodeChange(False, hexStr2)
+
+                result = System.Text.Encoding.UTF8.GetString(byteStr2)
+
+                If result.Contains("�") Then
+                    '文字化けされていたらまた変更する
+                    OutPutTextBox.Text &= System.Text.Encoding.UTF8.GetString(CharCodeChange(True, hexStr2))
+                Else
+                    OutPutTextBox.Text &= result
+                End If
+
+                count = 0
+                hexStr2 = ""
+            End If
+
+        Next
+
+    End Sub
+
+    ''' <summary>
+    ''' 暗号化、復号化に応じて文字コードを変更する
+    ''' </summary>
+    Private Function CharCodeChange(ByVal mojibake As Boolean, ByVal hexStr2 As String)
+
+        '分割した文字を格納
+        Dim moji4 As String()
+
+        Dim hexStr3 As String
+        Dim byteStr3 As Byte()
+        Dim decStr As Integer = 0
+        Dim count2 As Integer = 0
+
+        '10進数に変換
+        decStr = Convert.ToInt32(hexStr2, 16)
+
+        '最初の暗号化、復号化で文字化けした場合
+        If mojibake = True Then
+            '復号化の時は数値を引く
+            If isDec = True Then
+                decStr -= 205
+            Else
+                decStr += 205
+            End If
+        Else
+            If isDec = True Then
+                decStr -= 13
+            Else
+                decStr += 13
+            End If
+        End If
+
+        '16進数に変換
+        hexStr3 = Hex(decStr)
+
+        'カンマで区切る
+        For j As Integer = 0 To hexStr3.Length - 2
+            If j Mod 2 = 1 Then
+                '追加したカンマもカウントする
+                count2 += 1
+                hexStr3 = hexStr3.Insert(j + count2, ",")
+            End If
+        Next
+        count2 = 0
+
+        moji4 = hexStr3.Split(","c)
+
+        '暗号化した文字をバイトに変換
+        byteStr3 = New Byte(2) {}
+        For k = 0 To moji4.Length - 1
+            byteStr3(k) &= CByte(Convert.ToInt32(moji4(k), 16))
+        Next
+
+        Return byteStr3
+
+    End Function
+
+
+    ''' <summary>
+    ''' 暗号化ボタンをクリックした時
+    ''' </summary>
+    Private Sub Encryption_Button_Click(sender As Object, e As EventArgs) Handles Encryption_Button.Click
+
+        isDec = False
+        '暗号化を行う
+        EncDec()
+
+    End Sub
+
+    ''' <summary>
+    ''' 復号化ボタンをクリックした時
+    ''' </summary>
+    Private Sub Decryption_Button_Click(sender As Object, e As EventArgs) Handles Decryption_Button.Click
+
+        isDec = True
+        '復号化を行う
+        EncDec()
+
+    End Sub
+
+End Class
